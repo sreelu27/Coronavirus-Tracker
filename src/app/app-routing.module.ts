12fa035f3084b2fbb8d7abc1e1b06d8cda9f3bdc
sgeetha2 <sgeetha2@its.jnj.com>
2020-04-10 18:48:22 +0100

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,12 @@ const routes: Routes = [
   path:'home', component:HomeComponent
  },
  {
+  path:'', redirectTo:'home',pathMatch:'full'
+},
+{
+  path:'**', redirectTo:'home',pathMatch:'full'
+},
+ {
    path:'countries', component:CountriesListComponent
  },
  {
